3cef04bb2bb6b0018702c29e883cca345342b498
John Bass <CEO@johnny-airlines.co.uk>
2026-02-13 21:12:03 +0000

    Tester thingy, better now

--- tester.py
+++ tester.py
@@ -1,34 +1,54 @@
 import subprocess
 import sys
-from py_markdown_table.markdown_table import markdown_table
+from tabulate import tabulate
+import json
 
-def checkFile(program):
+def checkFile(program,update = False):
     global results
-    with open("./out.log","w") as f:
-            process = subprocess.Popen(["bash","./tester.sh",program],shell=False,stdout=f)
-            process.wait()
-    with open("./out.log","r") as f:
-        result = [i.rstrip() for i in f.readlines()]
-        if len(result) == 1:
-            print(f"{program} failed, took longer than 5m")
-            results.append({"Problem":i,"result":f"{program} failed, took longer than 5m"})
+    global i
+    try:
+        with open("./out.log","w") as f:
+            process = subprocess.run(["bash","./tester.sh",program],shell=False,stdout=f,timeout=300)
+        with open("./out.log","r") as f:
+            result = [i.rstrip() for i in f.readlines()]
+        if process.returncode == 1:
+            print(f"{program} failed, wrong value")
+            formattedRes = [i,"❌","-","Wrong value"]
+        elif process.returncode != 0:
+            print(f"{program} failed with exit code {process.returncode}, probably crashed")
+            formattedRes = [i,"❌","-",f"Exit code: {process.returncode}"]
         elif int(result[0].split("m")[0]) < 1:
             print(f"{program} passed with time: {result[0].split("m")[0]}m, {result[0].split("m")[1]}")
-            results.append({"Problem":i,"result":f"{program} passed with time: {result[0].split("m")[0]}m, {result[0].split("m")[1]}"})
+            formattedRes = [i,"✅",f"{result[0].split("m")[0]}m, {result[0].split("m")[1]}","-"]
         else:
             print(f"{program} failed with time: {result[0].split("m")[0]}m, {result[0].split("m")[1]}, took longer than 1m")
-            results.append({"Problem":i,"result":f"{program} failed with time: {result[0].split("m")[0]}m, {result[0].split("m")[1]}, took longer than 1m"})
+            formattedRes = [i,"❌",f"{result[0].split("m")[0]}m, {result[0].split("m")[1]}", "took longer than 1m"]
+    except subprocess.TimeoutExpired:
+        print(f"{program} failed, took longer than 5m")
+        formattedRes = [i,"❌","-",f"Took longer than 5m"]
+    if update:
+        if i <= len(results):
+            results.pop(i-1)
+        results.insert(i-1,formattedRes)
+    else:
+        results.append(formattedRes)
 
 options = sys.argv
 if len(options) == 1:
     results = []
-    for i in range(1,17):
+    for i in range(1,18):
         checkFile(f"./John/{i}.py")
-    markdown = markdown_table(results).get_markdown()
-    with open("README.md","w") as f:
-        f.write("""# ProjectEuler
-My attempt at the ProjectEuler problems
-"""+markdown.strip("`"))
+    
 else:
-     checkFile(options[1])
-        
+    with open("./results.json","r") as f:
+        results = json.load(f)
+    i = int(options[1])
+    checkFile(f"./John/{options[1]}.py",True)
+
+with open("./results.json","w") as f:
+    json.dump(results,f)
+markdown = tabulate(results,["Problem","Result","Time","Reason"],tablefmt="github")
+with open("README.md","w") as f:
+    f.write("""# ProjectEuler
+My attempt at the ProjectEuler problems
+"""+markdown)
