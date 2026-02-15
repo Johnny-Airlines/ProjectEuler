46a13903244444dad45a143d853fd1950059546f
John Bass <CEO@johnny-airlines.co.uk>
2026-02-15 18:49:27 +0000

    Update 5.py to have much faster solution

--- John/5.py
+++ John/5.py
@@ -3,14 +3,21 @@ Problem 5
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 '''
 
-def isEvenlyDivisible(x):
-    for i in range(1,20+1):
-        if x % i != 0:
-            return False
-    return True
+def gcd(a,b):
+    while a != b:
+        if a > b:
+            a -= b
+        elif a < b:
+            b -= a
+    return a
 
+def lcm(a,b):
+   return int(abs(a*b)/gcd(a,b))
 
-num = 20
-while not isEvenlyDivisible(num):
-    num = num + 1
-print(num)
+def lcmMany(*args):
+    num = 1
+    for a in args:
+        num = lcm(num,a)
+    return num
+
+print(lcmMany(*tuple(range(1,21))))
