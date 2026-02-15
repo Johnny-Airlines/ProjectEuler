279d98370b15b713bbdbc3a92f3cca2c906c623e
John Bass <CEO@johnny-airlines.co.uk>
2026-02-15 21:05:19 +0000

    Change floyds to be maximise

--- John/18.py
+++ John/18.py
@@ -4,7 +4,7 @@ def getColumn(matrix,i):
 def prettyPrint(matrix):
     for row in matrix:
         print(row)
-
+'''
 triangle = """3
 7 4
 2 4 6
@@ -14,16 +14,48 @@ rows = []
 for row in badRows:
     row = row.split(" ")
     rows.append(row)
+
+rows.pop(0)
 print(rows)
-distances = [[9999999 for j in range(len(rows))] for i in range(len(rows))]
-for i in range(len(rows)):
-    row = rows[i]
-    for j in range(len(row)):
-        distance = row[j]
-        print(j)
-        distances[i][j] = int(distance) * -1
-        if i == j:
-            distances[i][j] = "-"
+j = 0
+for row in rows:
+    for x in row:
+        j += 1
+distances = [[float('inf') for j in range(j)] for i in range(j+1)]
+for i in range(len(distances)):
+    distances[i].insert(i,"-")
+print(distances)
+
+i = 0
+a = 0
+j = 0
+offset = 0
+while i < 4:
+    row = rows[a]
+    #j = 0
+    b = j
+    k = i+offset
+    running = True
+    while running:
+        if distances[i][k] == "-":
+            k += 1
+        else:
+            distances[i][k] = row[j]
+            j += 1
+            k += 1
+            if j >= b+2:
+                running = False
+    if row[j-1] == row[-1]:
+        i += 1
+        a += 1
+        j = 0
+        offset += len(row)
+    else:
+        print(row[j-1])
+        i += 1
+        j -=1
+        '''
+
 '''
 distances = [
  ["-",7,5,10,float('inf')],
@@ -33,6 +65,32 @@ distances = [
  [float('inf'),3,float('inf'),3,"-"]
 ]
 '''
+distances = [
+    ["-",7,4,float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf')],
+    [float('inf'),"-",float('inf'),2,4,float('inf'),float('inf'),float('inf'),float('inf'),float('inf')],
+    [float('inf'),float('inf'),"-",float('inf'),4,6,float('inf'),float('inf'),float('inf'),float('inf')],
+    [float('inf'),float('inf'),float('inf'),"-",float('inf'),float('inf'),8,5,float('inf'),float('inf')],
+    [float('inf'),float('inf'),float('inf'),float('inf'),"-",float('inf'),float('inf'),5,9,float('inf')],
+    [float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),"-",float('inf'),float('inf'),9,3],
+    [float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),"-",float('inf'),float('inf'),float('inf')],
+    [float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),"-",float('inf'),float('inf')],
+    [float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),"-",float('inf')],
+    [float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),float('inf'),"-"]
+]
+distances = [[float('-inf') if j==float('inf') else (j * 1 if j != "-" else "-" )for j in i] for i in distances]
+'''
+distances = [
+    ["-",float("inf"),-2,float("inf")],
+    [4,"-",3,float("inf")],
+    [float("inf"),float("inf"),"-",2],
+    [float("inf"),-1,float("inf"),"-"]
+]'''
+for i in range(len(distances)):
+    row = distances[i]
+    for j in range(len(row)):
+        if row[j] != "-" and row[j] != float('inf'):
+            distances[i][j] *= 1
+    
 prettyPrint(distances)
 
 for k in range(len(distances)):
@@ -42,7 +100,8 @@ for k in range(len(distances)):
         for j in range(len(distances)):
             if i != k and j != k and distances[i][j] != "-":
                 sum = row[i] + column[j]
-                if sum < distances[i][j]:
-                    distances[i][j] = sum
-
+                if sum > distances[j][i]:
+                    distances[j][i] = sum
 prettyPrint(distances)
+distances[0].remove("-")
+print(max(distances[0]))
