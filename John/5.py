a33539647531d5e27dcca72c939b3ae3d79f6cd8
John Bass <CEO@johnny-airlines.co.uk>
2026-02-15 19:05:54 +0000

    Improve 5.py to use more efficient gcd algorithm

--- John/5.py
+++ John/5.py
@@ -3,7 +3,8 @@ Problem 5
 What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 '''
 
-def gcd(a,b):
+#Euclid's Algorithm
+def slowerGcd(a,b):
     while a != b:
         if a > b:
             a -= b
@@ -11,8 +12,17 @@ def gcd(a,b):
             b -= a
     return a
 
+#Euclidean Algorithm
+def gcd(a,b):
+    if a == 0 or a==b:
+        return b
+    if a > b:
+        return gcd(a % b, b)
+    if a < b:
+        return gcd(b % a, a)
+
 def lcm(a,b):
-   return int(abs(a*b)/gcd(a,b))
+    return int(abs(a*b)/gcd(a,b))
 
 def lcmMany(*args):
     num = 1
