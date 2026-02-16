ac5be8478d599c68fab8538d9c41203df1e5a27c
John Bass <CEO@johnny-airlines.co.uk>
2026-02-16 12:31:50 +0000

    Update tester.sh to include the first 50 problems and rerun most tests

--- tester.sh
+++ tester.sh
@@ -3,26 +3,16 @@
 # 1. Table
 declare -A lookup
 lookup=(
-    ["233168"]="pass"       # 1: Multiples of 3 and 5
-    ["4613732"]="pass"      # 2: Even Fibonacci numbers
-    ["6857"]="pass"         # 3: Largest prime factor
-    ["906609"]="pass"       # 4: Largest palindrome product
-    ["232792560"]="pass"    # 5: Smallest multiple
-    ["25164150"]="pass"     # 6: Sum square difference
-    ["104743"]="pass"       # 7: 10001st prime
-    ["23514624000"]="pass"  # 8: Largest product in a series
-    ["31875000"]="pass"     # 9: Special Pythagorean triplet
-    ["142913828922"]="pass" # 10: Summation of primes
-    ["70600674"]="pass"     # 11: Largest product in a grid
-    ["76576500"]="pass"     # 12: Highly divisible triangular number
-    ["5537376230"]="pass"   # 13: Large sum
-    ["837799"]="pass"       # 14: Longest Collatz sequence
-    ["137846528820"]="pass" # 15: Lattice paths
-    ["1366"]="pass"       # 16: Power digit sum
-    ["21124"]="pass"        # 17: Number letter counts
-    ["1074"]="pass"         # 18: Maximum path sum I
-    ["171"]="pass"          # 19: Counting Sundays
-    ["648"]="pass"          # 20: Factorial digit sum
+    ["233168"]="pass" ["4613732"]="pass" ["6857"]="pass" ["906609"]="pass" ["232792560"]="pass"
+    ["25164150"]="pass" ["104743"]="pass" ["23514624000"]="pass" ["31875000"]="pass" ["142913828922"]="pass"
+    ["70600674"]="pass" ["76576500"]="pass" ["5537376230"]="pass" ["837799"]="pass" ["137846528820"]="pass"
+    ["1366"]="pass" ["21124"]="pass" ["1074"]="pass" ["171"]="pass" ["648"]="pass"
+    ["31626"]="pass" ["691020"]="pass" ["4179871"]="pass" ["2783915460"]="pass" ["4782"]="pass"
+    ["2496144"]="pass" ["21000"]="pass" ["26115"]="pass" ["27074400"]="pass" ["443839"]="pass"
+    ["127520431"]="pass" ["45228"]="pass" ["40730"]="pass" ["1968329"]="pass" ["7129162514264337593543950335"]="pass"
+    ["1492155"]="pass" ["748317"]="pass" ["932718654"]="pass" ["1035"]="pass" ["210500"]="pass"
+    ["4543901"]="pass" ["1627549360"]="pass" ["16695334890"]="pass" ["547"]="pass" ["153372"]="pass"
+    ["1476"]="pass" ["46693"]="pass" ["1000"]="pass" ["2408404"]="pass" ["4065424064"]="pass"
 )
 
 tmp_time=$(mktemp)
