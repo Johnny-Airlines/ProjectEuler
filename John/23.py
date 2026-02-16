9d522a972db2c2327576a31afcc28f5d8ce77785
John Bass <CEO@johnny-airlines.co.uk>
2026-02-16 18:02:04 +0000

    Solution for 23!

--- John/23.py
+++ John/23.py
@@ -4,12 +4,15 @@ Find the sum of all positive integers which can not be written as the sum of two
 All intgers greater than 28123 can be written as the sum of two abundant numbers.
 An anbundant number is such that the sum of its proper divisors is greater than itself.
 '''
-
 def isAbundant(n):
-    sum = 1
-    for i in range(2,int(n**0.5)+2):
+    sum = 0
+    for i in range(1,int(n**0.5)+1):
         if n % i == 0:
-            sum += i + int(n/i)
+            #print(i,n/i)
+            if i != n:
+                sum += i
+            if int(n/i) != n and i != n/i:
+                sum += int(n/i)
             if sum > n:
                 return True
     return False
@@ -19,13 +22,12 @@ for i in range(28123):
     if isAbundant(i):
         abundantNumbers.append(i)
 
-print(abundantNumbers)
 numbers = {i:True for i in range(28123)}
+
 for i in abundantNumbers:
     for j in abundantNumbers:
         if i+j in numbers:
             numbers[i+j] = False
-print(numbers)
 trueNums = [key if value else 0 for key,value in numbers.items()]
 trueNums = list(set(trueNums))
 sum = 0
--- README.md
+++ README.md
@@ -24,3 +24,4 @@ My attempt at the ProjectEuler problems
 |        20 | ✅        | 0m, 0.025s  | -                   |
 |        21 | ✅        | 0m, 0.091s  | -                   |
 |        22 | ✅        | 0m, 0.376s  | -                   |
+|        23 | ✅        | 0m, 14.344s | -                   |
