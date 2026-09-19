22390468dbf8189d9705abb60900e471c239de0b
John Bass <CEO@johnny-airlines.co.uk>
2026-09-19 10:03:00 +0100

    Problem 2 cleanup

--- haskell/2.hs
+++ haskell/2.hs
@@ -1,11 +1,11 @@
 -- Even fibonacci sum up to limit of fibonacci number
 fibonacci :: (Integral n) => n -> n
-fibonacci n = go n (0, 1, 0)
+fibonacci limit = go 0 1 0
   where
-    go !limit (!a, !b, !sum)
+    go !a !b !sum
       | b >= limit = sum
-      | even (a + b) = go limit (b, a + b, sum + a + b)
-      | otherwise = go limit (b, a + b, sum)
+      | even (a + b) = go b (a + b) (sum + a + b)
+      | otherwise = go b (a + b) sum
 
 result :: Integer
 result = fibonacci 4000000
