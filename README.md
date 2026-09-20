abbc2204a29874d13c3b27bd8a6fce49785e644d
John Bass <CEO@johnny-airlines.co.uk>
2026-09-20 18:03:32 +0100

    Problem 3 clean

--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 My attempt at the ProjectEuler problems
 |   Problem | Result   | Time       | Reason   |
 |-----------|----------|------------|----------|
-|         1 | ✅        | 0m, 0.004s | -        |
-|         2 | ✅        | 0m, 0.004s | -        |
+|         1 | ✅        | 0m, 0.003s | -        |
+|         2 | ✅        | 0m, 0.003s | -        |
 |         3 | ✅        | 0m, 0.004s | -        |
--- haskell/3.hs
+++ haskell/3.hs
@@ -2,20 +2,11 @@
 divisibleBy :: (Integral n) => n -> n -> Bool
 divisibleBy n d = n `mod` d == 0
 
--- Determines if an Integral is prime
-isPrime :: (Integral n) => n -> Bool
-isPrime n = go 2
-  where
-    go !factor
-      | factor * factor > n = True
-      | n `divisibleBy` factor = False
-      | otherwise = go (factor + 1)
-
 -- Largest prime factor
 largestPrimeFactor :: (Integral n) => n -> n
 largestPrimeFactor n = go n 2
   where
-    go n factor
+    go !n !factor
       | factor * factor > n = n
       | n `divisibleBy` factor = go (n `div` factor) 2
       | otherwise = go n (factor + 1)
